2179577898d5c555f5ecff10b3f33e90c0c62edb
comejtome <youngnoh.goh@gmail.com>
2020-06-05 08:59:35 +0900

    서버와 클라이언트 연결까지

--- server/index.js
+++ server/index.js
@@ -2,10 +2,10 @@
 const express = require('express');
 //function을 이용해서 새로운 app을 만들어줌
 const app = express();
-const port = 5000;
+
 const bodyParser = require('body-parser');
 //mongoURI를 가져오기 위해서, 암호화
-const config = require('./server/config/key');
+const config = require('../server/config/key');
 const cookieParser = require('cookie-parser');
 const { auth } = require('./middleware/auth');
 const { User } = require('./models/User');
@@ -27,6 +27,10 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello world!'));
+
+app.get('/api/hello', (req, res) => {
+  res.send('안녕하세요');
+});
 
 app.post('/api/users/register', (req, res) => {
   // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
@@ -104,5 +108,5 @@ app.get('/api/users/logout', auth, (req, res) => {
     });
   });
 });
-
+const port = 5000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
